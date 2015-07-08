741fec1364fb64964a0cedfd5283ecf171a93259
Steven Soloff <s_soloff@yahoo.com>
2015-07-08 13:12:01 -0400

    Add button to reevaluate each row in history table.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,41 @@
-function evaluate() {
+function addExpressionResult(response) {
+    'use strict';
+
+    var $expressionTextCell = $('<td>').text(response.expression.text);
+    var $expressionCanonicalTextCell = $('<td>').text(response.expression.canonicalText);
+    var $expressionResultTextCell = $('<td>').text(response.expressionResult.text);
+    var $expressionResultValueCell = $('<td>').text(response.expressionResult.value.toString());
+
+    var $reevaluateButton = $('<button>').text('Reevaluate').click(function () {
+        evaluate($expressionTextCell.text());
+    });
+    var $removeButton = $('<button>').text('Remove').click(function (event) {
+        $(event.target).closest('tr').remove();
+    });
+    var $actionsCell = $('<td>').append($reevaluateButton, $removeButton);
+
+    var $expressionResultRow = $('<tr>').append(
+        $expressionTextCell,
+        $expressionCanonicalTextCell,
+        $expressionResultTextCell,
+        $expressionResultValueCell,
+        $actionsCell
+    );
+    $('#expressionResults').prepend($expressionResultRow);
+}
+
+function clearExpressionText() {
+    'use strict';
+
+    $('#expressionText').val('');
+}
+
+function evaluate(expressionText) {
     'use strict';
 
     var request = {
         expression: {
-            text: getExpressionText()
+            text: expressionText
         },
         randomNumberGenerator: {
             name: $('#randomNumberGeneratorName').val()
@@ -23,13 +55,19 @@ function getExpressionText() {
     return expressionText;
 }
 
-function initialize() {
+function hideErrorMessage() {
     'use strict';
 
     $('#errorMessage').invisible();
+}
+
+function initialize() {
+    'use strict';
+
+    hideErrorMessage();
 
     $('#expressionForm').submit(function (event) {
-        evaluate();
+        evaluate(getExpressionText());
         event.preventDefault();
     });
 }
@@ -50,30 +88,18 @@ function processResponse(response) {
     'use strict';
 
     if (response.error) {
-        $('#errorMessage').text(response.error.message).visible();
+        showErrorMessage(response.error.message);
     } else {
-        $('#expressionText').val('');
-
-        var $expressionTextColumn = $('<td>').text(response.expression.text);
-        var $expressionCanonicalTextColumn = $('<td>').text(response.expression.canonicalText);
-        var $expressionResultTextColumn = $('<td>').text(response.expressionResult.text);
-        var $expressionResultValueColumn = $('<td>').text(response.expressionResult.value.toString());
-        var $removeHistoryRowButton = $('<button>').text('Remove').click(function (event) {
-            $(event.target).closest('tr').remove();
-        });
-        var $actionsColumn = $('<td>').append($removeHistoryRowButton);
-        var $expressionResultRow = $('<tr>').append(
-            $expressionTextColumn,
-            $expressionCanonicalTextColumn,
-            $expressionResultTextColumn,
-            $expressionResultValueColumn,
-            $actionsColumn
-        );
-        var $expressionResults = $('#expressionResults');
-        $expressionResults.prepend($expressionResultRow);
-
-        $('#errorMessage').invisible();
+        clearExpressionText();
+        addExpressionResult(response);
+        hideErrorMessage();
     }
+}
+
+function showErrorMessage(message) {
+    'use strict';
+
+    $('#errorMessage').text(message).visible();
 }
 
 function main() {
