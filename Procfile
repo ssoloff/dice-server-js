e2462bfa4c2e5e3ecb1363e571df5b7caa37db22
Steven Soloff <steven.soloff@gmail.com>
2017-01-15 21:46:08 -0500

    Allow encryption keys to be specified via environment
    Heroku will provide the encryption keys via the environment from the
    app's configuration variables.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node build/dist/server.js test/server/test-keys/private-key.pem test/server/test-keys/public-key.pem
+web: node build/dist/server.js
--- src/server/server.js
+++ src/server/server.js
@@ -13,12 +13,22 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
+function getKey (keyType, fileName, envValue) {
+  if (fileName) {
+    return fs.readFileSync(fileName)
+  } else if (envValue) {
+    return envValue
+  }
+
+  throw new Error(`${keyType} key not specified`)
+}
+
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(correlator())
 
-const privateKey = fs.readFileSync(process.argv[2])
-const publicKey = fs.readFileSync(process.argv[3])
+const privateKey = getKey('private', process.argv[2], process.env.DSJS_PRIVATE_KEY)
+const publicKey = getKey('public', process.argv[3], process.env.DSJS_PUBLIC_KEY)
 require('./routes')(app, privateKey, publicKey)
 
 app.listen(process.env.PORT || 3000)
