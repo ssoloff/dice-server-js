bc24531aeb7dcf75783010d5ea0612bec0110213
Steven Soloff <steven.soloff@gmail.com>
2017-02-14 15:08:06 -0500

    Add unit tests for routes module

---
+++ test/server/routes.spec.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2015-2017 Steven Soloff
+ *
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the MIT License (https://opensource.org/licenses/MIT).
+ * This software comes with ABSOLUTELY NO WARRANTY.
+ */
+
+'use strict'
+
+const express = require('express')
+const routes = require('../../src/server/routes')
+const securityTest = require('./test-support/security-test')
+
+describe('routes', () => {
+  it('should not throw an exception when provided a valid application', () => {
+    expect(() => {
+      const app = express()
+      routes(app, securityTest.getPrivateKey(), securityTest.getPublicKey())
+    }).not.toThrow()
+  })
+})
